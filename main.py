65520d739686de2fbb2f7a5d6b402a3447b456f8
archit kumar <archit@wpi.edu>
2023-03-15 20:12:53 -0500

    Build nameform using flask-wtf

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, url_for
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
+from dotenv import load_dotenv
 
 
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("secret_key")
 
+# name form
+class NameForm(FlaskForm):
+    name = StringField("Enter Name", validators=[DataRequired()])
+    submit_btn = SubmitField("Submit")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    form = NameForm()
+    if form.validate_on_submit():
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, name=session.get("name"))
